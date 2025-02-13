9015c0019ebe9ce8b32d88ff20ff4efdd712d734
Liam <liamandriemartin0@gmail.com>
2025-02-13 14:54:24 +0800

    aadded error handling

--- Weight_Converter_Program.py
+++ Weight_Converter_Program.py
@@ -1,6 +1,12 @@
 # Ask the user for its weight
 while True:
     weight = input("Please state your weight: ")
+    
+    try:
+        weight = float(weight)  # Convert input to float
+    except ValueError:
+        print("Invalid weight. Please enter a number.")
+        continue
 
 # Ask if the weight given is in Pounds (Lbs) or Kilograms (Kl)
     is_lbs_or_kl = input("What is the unit of the given weight? Enter 'L' for pounds or 'K' for kilograms: ")
@@ -11,11 +17,11 @@ while True:
 
 # If statements
 
-    if is_lbs_or_kl.upper() == "L":
+    if is_lbs_or_kl.upper() == "K":
         print(f"Your weight in pounds is {lbs_converter}")
         break
 
-    elif is_lbs_or_kl.upper() == "K":
+    elif is_lbs_or_kl.upper() == "L":
         print(f"Your weight in kilograms is {kilo_converter}")
         break
     else:
