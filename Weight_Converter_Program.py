b3e9c09cedeec52925d2e96c10752c7b0e9a9693
Liam <liamandriemartin0@gmail.com>
2025-02-13 14:42:09 +0800

    code implementation

---
+++ Weight_Converter_Program.py
@@ -0,0 +1,22 @@
+# Ask the user for its weight
+while True:
+    weight = input("Please state your weight: ")
+
+# Ask if the weight given is in Pounds (Lbs) or Kilograms (Kl)
+    is_lbs_or_kl = input("What is the unit of the given weight? Enter 'L' for pounds or 'K' for kilograms: ")
+
+# Add unit converters
+    lbs_converter = int(weight) * 2.205
+    kilo_converter = int(weight) / 2.205
+
+# If statements
+
+    if is_lbs_or_kl == "L":
+        print(f"Your weight in pounds is {lbs_converter}")
+        break
+
+    elif is_lbs_or_kl == "K":
+        print(f"Your weight in kilograms is {kilo_converter}")
+        break
+    else:
+        print("Invalid response, please try again.")
